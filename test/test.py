23917f6bd1101c9f9d4ffe94071bafe1835df731
moreno <morenobonaventura@gmail.com>
2015-04-10 00:11:04 +0100

    update all

--- test/test.py
+++ test/test.py
@@ -8,9 +8,7 @@ SparseD = nl.sparse_distance_matrix(G)                         #compute the Spar
 
 new_edges = [(randrange(N),randrange(N)) for c in range(100)]
 for i,j in new_edges:                                 
-    if G.has_edge(i,j) or i==j:
-        continue
-    print 'adding edge (%s,%s)'%(i,j)
-    nl.update_distance_matrix(G,SparseD,i,j,mode='add')    #add edges and update Distance Matrix
+    print 'adding edge (%s,%s), updating Distance Matrix...'%(i,j)
+    nl.update_distance_matrix(G,SparseD,i,j,mode='add')        #add edges and update Distance Matrix
 
-print SparseD[5][12]
+print SparseD[5][12]                                           #accessing distance values
